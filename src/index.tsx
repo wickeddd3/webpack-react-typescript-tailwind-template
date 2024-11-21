226607a6ce9f9a8b6fc69fce06c3bcf218d5ae67
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-22 02:49:39 +0800

    refactor: rename globals.css to index.css and move to assets/css

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import "./styles/globals.css";
+import "./assets/css/index.css";
 import { App } from "./components/App";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
