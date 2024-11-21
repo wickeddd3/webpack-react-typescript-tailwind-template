42ce7c6e0bf6c8c12dec5dba70209211f356b8b5
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-22 03:37:03 +0800

    add: App sample content

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export const App = () => {
-  return <h1 className="bg-red-500">Hello World</h1>;
+  return (
+    <>
+      <h1 className="bg-red-500">Hello World</h1> <Button>Button</Button>
+    </>
+  );
 };
